fe5e3218e5818bf57db01f9e70b5131c3e4c6ca2
MuntahaHaider <160015309+MuntahaHaider@users.noreply.github.com>
2024-07-03 17:27:59 +0500

    Assignment5
    Assignment5

---
+++ sadia assignment 5/assignment5/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+function App() {
+  return(
+<>
+     <div>           
+      <h1>Salam React js</h1>
+      <p>React is aptly named because: It "reacts" quickly to changes without reloading the whole page. It uses tvirtual DOM to efficiently update parts of a webpage. It's built around components that "react" and update </p>
+      <img src="/vite.svg" alt="" />
+    
+        </div>
+  </>
+  )
+}
+
+export default App
